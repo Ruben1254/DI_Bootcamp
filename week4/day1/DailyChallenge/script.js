bacf280614150a9a54e8f680eb510fb90e250c45
Ruben1254 <reuven.tsbn@gmail.com>
2022-07-11 00:03:07 +0300

    almost2

---
+++ week4/day1/DailyChallenge/script.js
@@ -0,0 +1,33 @@
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
+
+gameInfo.forEach(function newArray (username, index){
+  let usernames = [""];
+  usernames = gameInfo[index]["username"];
+});
+console.log(usernames)
+
