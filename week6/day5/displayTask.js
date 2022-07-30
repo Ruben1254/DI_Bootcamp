2eb8ef46b51717e6956f14628ccfbabbe03760dd
Ruben1254 <reuven.tsbn@gmail.com>
2022-07-31 01:46:34 +0300

    'almost'

--- week6/day5/displayTask.js
+++ week6/day5/displayTask.js
@@ -9,19 +9,38 @@
 	let p = document.createElement('p');
 	let time = document.createElement('p');
 
+	let today = new Date();
+	let taskDate = new Date(start);
+	let remainingDay = Math.round((taskDate.getTime() - today.getTime()) / (1000 * 3600 * 24));
+	let nodeDay = document.createTextNode(`Remaining time: ${remainingDay} days`);
+
+	let year = start.slice(0,10)
+	let hour = start.slice(11,16)
+	console.log(remainingDay)
+
 	let nom = document.createTextNode(name);
-	let des = document.createTextNode(`Description: ${description}`);
-	let hour = document.createTextNode(start);
+	let dateHour = document.createTextNode(`Date: ${year} at ${hour}`);
+
 
 	h4.appendChild(nom);
-	p.appendChild(des);
-	time.appendChild(hour);
+	p.appendChild(dateHour);
+	time.appendChild(nodeDay);
 
 	container.appendChild(h4);
 	container.appendChild(p);
 	container.appendChild(time);
 
-	document.body.appendChild(container)
+	document.body.appendChild(container);
+
+	container.addEventListener('click', displayDes);
 
 	})
 })()
+
+
+function displayDes(e) {
+	console.log(e.target)
+}
+
+
+
