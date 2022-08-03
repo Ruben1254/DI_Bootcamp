ce4593cb2721b3f07eb118cda3edeb490127cd74
Ruben1254 <reuven.tsbn@gmail.com>
2022-08-03 15:25:30 +0300

    daily

--- week7/day1/DailyChallenge/main.js
+++ week7/day1/DailyChallenge/main.js
@@ -1,5 +1,16 @@
 const largeNumber = 365;
 
+const dateTime = () => {
+	let date = new Date();
+	return date 
+}
+
+const invoke = dateTime();
+
+
 module.exports = {
-	largeNumber: largeNumber
+	largeNumber: largeNumber,
+	date: invoke
 };
+
+
--- week7/day1/DailyChallenge/script.js
+++ week7/day1/DailyChallenge/script.js
@@ -7,9 +7,18 @@ console.log(a + b);
 
 let http = require("http");
 
-const server = http.createServer(options, function (req, res) {
+const server = http.createServer(function (req, res) {
 	console.log("I'am listening");
 	res.setHeader("Content-Type","text/html");
-	res.end('<html><body><p>My Module is main</p><br><h1>Hi there at the frontend</h1></body></html>');	
+	res.end('<html><body><h1>Hi there at the frontend</h1></body></html>');	
 });
 server.listen(3000);
+
+const dateT = main.date;
+
+const server2 = http.createServer(function (req,res){
+	console.log("server2");
+	res.setHeader("Content-Type","text/html");
+	res.end(`<html><body><p>The date and the time currently ${dateT}</p></body></html>`)
+});
+server2.listen(8080);
