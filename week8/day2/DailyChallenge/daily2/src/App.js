ccccf79d83797017652ab4b1af5d5e5235ce18dd
Ruben1254 <reuven.tsbn@gmail.com>
2022-08-12 16:20:46 +0300

    dayli2

---
+++ week8/day2/DailyChallenge/daily2/src/App.js
@@ -0,0 +1,14 @@
+import Voting from './Voting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Voting />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ week8/day2/DailyChallenge/daily2/src/Voting.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Voting extends React.Component{
+        constructor() {
+          super();
+          this.state = {
+            languages : [
+                {name: "Php", votes: 1},
+                {name: "Python", votes: 0},
+                {name: "JavaSript", votes: 0},
+                {name: "Java", votes: 0}
+            ]
+          }
+        }
+      
+        handleClick = (lang) => {
+          lang.votes++;
+          this.setState({languages:[...this.state.languages]})
+        }
+      
+        render(){
+          return(
+            <div>
+              <h1>Voting App</h1>
+              {
+                this.state.languages.map( (item,i) =>{
+                  return(
+                    <div key={i}>
+                      {item.name} {item.votes}
+                      <button onClick={()=>this.handleClick(item)}>Vote</button>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          )
+        }
+      }
+
+export default Voting
